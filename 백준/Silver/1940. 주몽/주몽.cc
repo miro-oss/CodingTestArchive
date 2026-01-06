b01b56d35097332ca927e3a60a74bb75f406bead
miro <tldms822@naver.com>
2026-01-06 21:25:35 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N, 0);
+    
+    for(int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    
+    sort(A.begin(), A.end());
+    
+    int count = 0;
+    int i = 0;
+    int j = N-1;
+    
+    while(i < j){
+        if(A[i] + A[j] > M){
+            j--;
+        }else if(A[i] + A[j] < M){
+            i++;
+        }else{
+            count++;
+            i++;
+            j--;
+        }
+    }
+    cout << count << "\n";
+}
