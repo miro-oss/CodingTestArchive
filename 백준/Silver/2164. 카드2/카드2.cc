d63c5cfafd25d30797de4cb8c2df76a024c7eae3
miro <tldms822@naver.com>
2026-01-07 22:12:51 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    queue<int> q;
+
+    for(int i = 1; i <= n; i++){
+        q.push(i);
+    }
+
+    for(int i = 1; i<n ; i++){
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    cout << q.front();
+ }
