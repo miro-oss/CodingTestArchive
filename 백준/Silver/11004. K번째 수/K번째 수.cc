cd83a97a970373bdf904ada8a1e64742a82d6eb8
miro <tldms822@naver.com>
2026-01-08 21:52:56 +0900

    [Silver V] Title: K번째 수, Time: 632 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    vector<int> A(N);
+    for(int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    nth_element(A.begin(), A.begin() + (K-1), A.end());
+    
+    cout << A[K-1] << "\n";
+
+}
