33586c9cdca816cfdc5ebe4ef4b68e6561f042f7
miro <tldms822@naver.com>
2026-03-22 16:36:50 +0900

    [Gold III] Title: 선물 전달, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/1947. 선물 전달/선물 전달.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+static int N;
+static int mod = 1000000000;
+static long D[1000001];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N ;
+    D[1] = 0;
+    D[2] = 1;
+
+    for(int i = 3; i <= N; i++){
+        D[i] = (i-1) * (D[i-1] + D[i-2]) % mod;
+    }
+    cout << D[N];
+}
