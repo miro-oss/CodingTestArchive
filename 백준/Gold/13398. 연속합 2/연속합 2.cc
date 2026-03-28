e89a89b22d9d17bd954e2170c2f2c4b5598b2c94
miro <tldms822@naver.com>
2026-03-28 16:28:02 +0900

    [Gold IV] Title: 연속합 2, Time: 40 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+static int N;
+static vector<int> A, L, R;
+
+int main(){
+    cin >> N;
+    A.resize(N);
+    
+    for(int i = 0; i < N ; i++){
+        cin >> A[i];
+    }
+    
+    L.resize(N);
+    L[0] = A[0];
+    int result = L[0];
+    
+    for(int i = 1; i < N; i++){
+        L[i] = max(A[i], L[i - 1] + A[i]);
+        result = max(result, L[i]);
+    }
+    
+    R.resize(N);
+    R[N - 1] = A[N - 1];
+    
+    for(int i = N - 2; i >= 0; i--){
+        R[i] = max(A[i], R[i + 1] + A[i]);
+    }
+    
+    for(int i = 1; i < N - 1; i++){
+        int temp = L[i - 1] + R[i + 1];
+        result = max(result, temp);
+    }
+    cout << result << '\n';
+}
