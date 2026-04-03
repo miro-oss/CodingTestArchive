6d06e2379e24feab6eb502b9c7614cbb53a7ec0b
LEE SIEUN <tldms822@naver.com>
2026-04-03 23:18:03 +0900

    [Gold I] Title: 외판원 순회, Time: 24 ms, Memory: 6116 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+static int INF = 1000000 * 16 + 1;
+static int N;
+static int W[16][16];
+static int D[16][(1 << 16)];
+int tsp(int c, int v);
+
+int main(){
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> W[i][j];
+        }
+    }
+    cout << tsp(0, 1) << '\n';
+}
+
+int tsp(int c, int v){
+    if(v == (1 << N) - 1){
+        return W[c][0] == 0 ? INF : W[c][0];
+    }
+    if(D[c][v] != 0){
+        return D[c][v];
+    }
+    int min_val = INF;
+    for(int i = 0; i < N; i++){
+        if((v & (1 << i)) == 0 && W[c][i] != 0){
+            min_val = min(min_val, tsp(i, (v | (1 << i))) + W[c][i]);
+        }
+    }
+            D[c][v] = min_val;
+        return D[c][v];
+}
